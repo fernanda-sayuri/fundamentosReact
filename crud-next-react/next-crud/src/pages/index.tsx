b06b6c55a7490e12495bb06ab6307159fb02bc8d
fernanda-sayuri <fernanda.sayuri.ito@accenture.com>
2022-06-18 21:14:07 -0300

    configurando next e tailwind-css

---
+++ crud-next-react/next-crud/src/pages/index.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className={`flex h-screen justify-center items-center bg-gradient-to-r from-purple-500 via-yellow-500 to-blue-500`}>
+      <span className="text-4xl">Hello</span>
+    </div>
+  )
+}
